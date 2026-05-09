6e4949ef18c238aef0a5a8c42e8d768ca1728648
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-05-09 08:55:37 +0300

    доставка фон

--- src/components/Delivery/sections/Features.jsx
+++ src/components/Delivery/sections/Features.jsx
@@ -5,15 +5,27 @@ export default function Features() {
     <section className="features-delivery">
       <div className="container grid-3">
         <div>
-          <h3>🌿</h3>
+          <img
+            src="/icon/package.png"
+            alt="Упаковка"
+            className="feature-icon"
+          />
           <p>Специальная упаковка — растение не повредится в пути</p>
         </div>
         <div>
-          <h3>🚚</h3>
+          <img
+            src="/icon/shipping.png"
+            alt="Доставка"
+            className="feature-icon"
+          />
           <p>Доставка курьером до двери</p>
         </div>
         <div>
-          <h3>📦</h3>
+          <img
+            src="/icon/sending.png"
+            alt="Отправка"
+            className="feature-icon"
+          />
           <p>Отправляем в день заказа</p>
         </div>
       </div>
